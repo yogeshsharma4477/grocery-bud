7784f099ae9b56d884a93c21f78c92b35610f904
Yogeshwar Sharma <54261899+yogeshsharma4477@users.noreply.github.com>
2021-07-15 01:04:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ function App() {
           <input
             type='text'
             className='grocery'
-            placeholder='e.g eggs'
+            placeholder='e.g utter milk'
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
